29c5436ede6e18c2d6357109474b7625c2c23791
Bhawna Adhikari <bhawna.ad05@gmail.com>
2021-08-08 16:37:45 +0530

    added connecting feature

--- Connections.py
+++ Connections.py
@@ -18,4 +18,19 @@ submit = driver.find_element_by_xpath("//button[@type='submit']")
 driver.execute_script("arguments[0].click();", submit)
 time.sleep(2)
 
+# loading connections page
+driver.get("https://www.linkedin.com/search/results/people/?origin=SWITCH_SEARCH_VERTICAL")
+time.sleep(2)
+
+all_buttons = driver.find_elements_by_tag_name("button")
+connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
+
+for btn in connect_buttons:
+    driver.execute_script("arguments[0].click();", btn)
+    time.sleep(2)
+    send = driver.find_element_by_xpath("//button[@aria-label = 'Send now']")
+    driver.execute_script("arguments[0].click();", send)
+    close = driver.find_element_by_xpath("//button[@aria-label = 'Dismiss']")
+    driver.execute_script("arguments[0].click();", close)
+    time.sleep(2)
 
